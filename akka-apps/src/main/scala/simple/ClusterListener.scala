2dd2a2dac2d0d3493f099679c3e0ead46b2e683f
Yury Gribkov <yury.gribkov@gmail.com>
2018-05-14 16:30:02 -0400

    Print cluster status for each node

--- akka-apps/src/main/scala/simple/ClusterListener.scala
+++ akka-apps/src/main/scala/simple/ClusterListener.scala
@@ -1,14 +1,17 @@
 package simple
 
+import akka.actor.{Actor, ActorLogging, Props}
 import akka.cluster.Cluster
 import akka.cluster.ClusterEvent._
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.cluster.MemberStatus.{Up, WeaklyUp}
+
+import scala.collection.immutable.SortedMap
 
 object ClusterListener {
   def props(): Props = Props.create(classOf[ClusterListener], () => new ClusterListener)
 }
 
-class ClusterListener extends Actor with ActorLogging {
+final class ClusterListener extends Actor with ActorLogging {
 
   val cluster = Cluster(context.system)
 
@@ -19,19 +22,62 @@ class ClusterListener extends Actor with ActorLogging {
   }
   override def postStop(): Unit = cluster.unsubscribe(self)
 
+  val thisHost = context.system.settings.config.getString("akka.remote.netty.tcp.hostname")
+
+  var nodes: SortedMap[String, String] = SortedMap.empty
+  var leader: Option[String] = None
+
+
+  private def printStatus(): Unit = {
+    val nodeStatuses = nodes.foldLeft(List.empty[String]) { case (b, (k, v)) => f"$k%5s:$v%11s" :: b }
+    val prefix = leader.getOrElse("     ")
+    println(f"$thisHost%5s|" + prefix + "|" + nodeStatuses.reverse.mkString("|"))
+  }
+
+
   def receive = {
+    case msg @ MemberUp(member) =>
+      nodes += member.address.host.get -> "up"
+      printStatus()
+
+    case msg @ MemberLeft(member) =>
+      nodes += member.address.host.get -> "left"
+      printStatus()
+
+    case msg @ MemberExited(member) =>
+      nodes += member.address.host.get -> "exited"
+      printStatus()
+
+    case msg @ MemberJoined(member) =>
+      nodes += member.address.host.get -> "joined"
+      printStatus()
+
+    case msg @ MemberRemoved(member, previousStatus) =>
+      nodes += member.address.host.get -> "removed"
+      printStatus()
+
+    case msg @ MemberWeaklyUp(member) =>
+      nodes += member.address.host.get -> "weakly-up"
+      printStatus()
+
+    case msg @ ReachableMember(member) if member.status == Up =>
+      nodes += member.address.host.get -> "up"
+      printStatus()
+
+    case msg @ ReachableMember(member) if member.status == WeaklyUp =>
+      nodes += member.address.host.get -> "weakly-up"
+      printStatus()
+
+    case msg @ UnreachableMember(member) =>
+      nodes += member.address.host.get -> "unreachable"
+      printStatus()
+
+    case msg @ LeaderChanged(newLeader) =>
+      this.leader = newLeader.flatMap(_.host)
+      printStatus()
+
     case evt ⇒
       println("Cluster Event: " + evt)
-
-//    case MemberUp(member) ⇒
-//      log.info("Member is Up: {}", member.address)
-//    case UnreachableMember(member) ⇒
-//      log.info("Member detected as unreachable: {}", member)
-//    case MemberRemoved(member, previousStatus) ⇒
-//      log.info(
-//        "Member is Removed: {} after {}",
-//        member.address, previousStatus)
-//    case _: MemberEvent ⇒ // ignore
   }
 }
 
